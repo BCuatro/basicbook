7ec4dff129f90474c77a0561e5c56ab4cc4c12d3
Eric Balfour <egbalfour@gmail.com>
2022-07-07 16:50:40 -0400

    demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,5 @@ User.delete_all
 blackpanther = User.create!(username:"Black Panther", first_name: "Chadwick", last_name: "Boseman", email: "kingchalla@wakanda.com", password: "wakanda", gender: "Male", bio: "Wakanda Forever!", location: "Oakland,CA")
 spiderman = User.create!(username:"Spiderman", first_name: "Peter", last_name: "Parker", email: "daspiderman@webmail.com", password: "zendaya", gender: "Male", bio: "With great powers come great responsiblity", location: "NYC")
 
-post1 = Post.create!(body: "Good Morning Y'all", author_id: 31, profile_id: 32 )
-post2 = Post.create!(body: "Helloworld", author_id: 30, profile_id: 33 )
+# post1 = Post.create!(body: "Good Morning Y'all", author_id: 31, profile_id: 32 )
+# post2 = Post.create!(body: "Helloworld", author_id: 30, profile_id: 33 )
